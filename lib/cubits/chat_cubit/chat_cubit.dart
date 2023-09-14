d83ecd635e40187f2df6855413525138e37a1573
osa989 <osamagamal>
2023-09-14 21:56:03 +0300

    adding logic : send and get message methods

--- lib/cubits/chat_cubit/chat_cubit.dart
+++ lib/cubits/chat_cubit/chat_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:chatt_app/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitial());
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kMessagesCollection);
+  void sendMessage({required String message, required String email}) {
+    messages.add({kMessage: message, kCreatedAt: DateTime.now(), 'id': email});
+  }
+
+  void getMessage() {
+    messages.orderBy(kCreatedAt, descending: true).snapshots().listen((event) {
+      emit(ChatSuccess());
+    });
+  }
 }
