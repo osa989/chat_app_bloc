7b1aa84418be7a86ac147e59ff4989971aaf2159
osa989 <osamagamal>
2023-09-15 06:43:21 +0300

    creating auth bloc

---
+++ lib/blocs/auth_bloc/auth_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AuthEvent>((event, emit)async {
+      if (event is LoginEvent) {
+         emit(LoginLoading());
+
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: event.email, password: event.password);
+      emit(LoginSuccess());
+    } on FirebaseAuthException catch (ex) {
+      if (ex.code == 'wrong-password') {
+        emit(LoginFailure(errMessage: "password is incorrect"));
+      } else if (ex.code == 'user-not-found') {
+        emit(LoginFailure(errMessage: "No user found for that email"));
+      }
+    } catch (e) {
+      emit(LoginFailure(errMessage: "something went wrong"));
+    }
+      }
+    });
+  }
+}
---
+++ lib/blocs/auth_bloc/auth_state.dart
@@ -0,0 +1,23 @@
+part of 'auth_bloc.dart';
+
+@immutable
+sealed class AuthState {}
+
+final class AuthInitial extends AuthState {}
+final class RegisterLoading extends AuthState {}
+
+final class RegisterSuccess extends AuthState {}
+
+final class RegisterFailure extends AuthState {
+final  String errMessage;
+  RegisterFailure({required this.errMessage});
+}
+
+final class LoginSuccess extends AuthState {}
+
+final class LoginLoading extends AuthState {}
+
+final class LoginFailure extends AuthState {
+  final String errMessage;
+  LoginFailure({required this.errMessage});
+}
